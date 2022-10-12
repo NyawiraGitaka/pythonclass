c7626935f8ca252552ac99c0a62aeab296c4462d
Nyawira Gitaka <nyawira.gitaka@gmail.com>
2022-10-12 08:12:41 +0300

    python file

---
+++ intro.py
@@ -0,0 +1,8 @@
+print("Hello World! My name is Zeus, I am a very good boy.")
+e=15
+k=18
+print (e+k)
+b=int(input("Enter the first number"))
+d=int(input("Enter the second number"))
+sum=b+d
+print(sum)
